8205eadd5e78ab8bf381e0e121d93f2a35245bdc
keval710 <155943442+keval710@users.noreply.github.com>
2024-02-06 10:30:30 +0530

    Delete main directory

--- main/dist/controller/Response.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Responses = void 0;
-const Responses = (req, res) => {
-};
-exports.Responses = Responses;
--- main/dist/controller/Sectionmatch.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SectionMatch = void 0;
-const SectionMatch = (req, res) => {
-    //@ts-ignore
-    return res.json({ TotalPoint: req.point });
-};
-exports.SectionMatch = SectionMatch;
